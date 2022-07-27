198697ccdcf108373653dfb4a62006d91e1670fe
myeongyunChoi <cmy2257@naver.com>
2022-07-27 18:33:01 +0900

    2_main

--- js/main.js
+++ js/main.js
@@ -3,18 +3,27 @@ window.addEventListener("scroll", function () {
     // const cont = document.getElementById("introduce");
 
     const txt_line = document.querySelector(".text_line");
-    const menu_txt = document.getElementById("meun_txt")
     let win_top = window.scrollY;
-    
-    if (win_top > 100){
-        txt_line.style.opacity="0";
-    }else{
-        txt_line.style.opacity="1";
+
+    if (win_top > 100) {
+        txt_line.style.opacity = "0";
+    } else {
+        txt_line.style.opacity = "1";
     }
 
-    // if(win_top > $he){
-        
-    // }
+    const guide_top = document.getElementById("introduce").offsetTop;
+    const mov_letter = document.querySelectorAll(".guide_big_box > h2");
+
+
+    for (let l = 0; l < mov_letter.length; l++) {
+        if (win_top > guide_top / 2) {
+                mov_letter[0].style.transform = `translateX(-100%)`;
+                mov_letter[1].style.transform = `translateX(50%)`;
+                mov_letter[2].style.transform = `translateX(100%)`;
+        } else {
+            mov_letter[l].style = "";
+        }
+    }
 
     const slide_wrap = document.getElementById("hotell_more");
     const slide = document.querySelector(".slide");
@@ -45,12 +54,30 @@ window.addEventListener("scroll", function () {
         slide.style.transform = `translateX(${mov}px)`;
     } else {
         slide.classList.remove("fix");
-        mov=0;
+        mov = 0;
         slide.style.transform = `translateX(${mov}px)`;
     }
 
     if (win_top >= room_top) {
         slide.classList.remove("fix");
+    }
+
+
+    const one_hei = document.querySelector("#main_view").clientHeight;
+    const menu_txt = document.querySelectorAll(".menu_txt");
+
+    for (let i = 0; i < menu_txt.length; i++) {
+        if (win_top > one_hei) {
+            menu_txt[i].classList.add("black");
+        } else {
+            menu_txt[i].classList.remove("black");
+        }
+
+        if (win_top > s_top) {
+            menu_txt[i].classList.add("white");
+        } else {
+            menu_txt[i].classList.remove("white");
+        }
     }
 
 })
