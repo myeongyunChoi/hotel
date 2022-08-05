78f541810a1f65c30c3979c349c1ce018108c155
myeongyunChoi <104118233+myeongyunChoi@users.noreply.github.com>
2022-08-05 17:01:12 +0900

    main_6.5

--- js/main.js
+++ js/main.js
@@ -6,16 +6,22 @@ window.addEventListener("scroll", function () {
     const txt_line = document.querySelector(".text_line");
     let win_top = window.scrollY;
 
-    if (win_top > 100) {
+    if (win_top > 0) {
         txt_line.style.opacity = "0";
     } else {
         txt_line.style.opacity = "1";
     }
 
-    //
     const guide_top = document.getElementById("introduce").offsetTop;
-
     const mov_letter = document.querySelectorAll(".guide_big_box > h2");
+    // const main_view = document.querySelector("#main_view");
+
+
+    // if (win_top > guide_top/2){
+    //     main_view.style.width = "200%";
+    // }else{
+    //     main_view.style= "";
+    // }
 
     for (let l = 0; l < mov_letter.length; l++) {
         if (win_top > guide_top / 2) {
@@ -40,21 +46,27 @@ window.addEventListener("scroll", function () {
     const guide_img1 = document.querySelector(".guide_img1").offsetTop;
     const guide_img1_top = guide_img1 + guide_top;
     const guide_img_box1 = document.querySelector(".guide_img1 > div");
+    const guide_over_1 = document.querySelector(".guide_over_1");
 
     const guide_img2 = document.querySelector(".guide_img2").offsetTop;
     const guide_img2_top = guide_img2 + guide_top;
     const guide_img_box2 = document.querySelector(".guide_img2 > div");
+    const guide_over_2 = document.querySelector(".guide_over_2");
 
     if(win_top > guide_img1_top * 0.75){
         guide_img_box1.classList.remove("op_2");
+        guide_over_1.style.transform = "translateY(-100%)";
     }else{
         guide_img_box1.classList.add("op_2");
+        guide_over_1.style.transform = "translateY(0%)";
     }
 
     if(win_top > guide_img2_top * 0.85){
         guide_img_box2.classList.remove("op_2");
+        guide_over_2.style.transform = "translateY(-100%)";
     }else{
         guide_img_box2.classList.add("op_2");
+        guide_over_2.style.transform = "translateY(0%)";
     }
 
 
